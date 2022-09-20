6707cb65ef9923a9985ebd3e192cf7382d983c65
«Ann» <annapt69@mail.ru>
2022-09-20 19:24:42 +0300

    task4_2

--- task4/Program.cs
+++ task4/Program.cs
@@ -4,13 +4,12 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 while (count <= number)
 {
-    if (number = number%2)
+    if (number % 2 == 0)
     Console.Write($"{number}");
-    count = count + 1; 
 }
-
